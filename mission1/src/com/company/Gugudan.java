36d2b66ce12edd3832867501ee4b2a7ad34ae5fd
Nayeon <kimnayeon0108@users.noreply.github.com>
2020-11-04 11:32:19 +0900

    new project

---
+++ mission1/src/com/company/Gugudan.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Gugudan {
+    public int start;
+    public int end;
+
+    public void enter(){
+        Scanner s = new Scanner(System.in);
+        System.out.println("enter start num.");
+        start = s.nextInt();
+        System.out.println("enter end num");
+        end = s.nextInt();
+        s.close();
+    }
+
+    public void printGugudan(){
+        for(int i = start; i <= end; i++){
+            System.out.println(i + "단");
+            for(int j = 1; j < 10; j++ ){
+                System.out.println(i + " * " + j + " = " + (i * j));
+            }
+            System.out.println("--------");
+        }
+    }
+
+    public static void main(String[] args) {
+        Gugudan gugu = new Gugudan();
+
+        gugu.enter();
+        gugu.printGugudan();
+    }
+
+}
+
---
+++ mission1/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        Gugudan gugu = new Gugudan();
+
+        gugu.enter();
+        gugu.printGugudan();
+    }
+}
