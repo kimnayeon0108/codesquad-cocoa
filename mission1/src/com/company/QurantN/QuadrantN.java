2b868ba203359cb751d6fcf825c63e8b16468275
Nayeon <kimnayeon0108@users.noreply.github.com>
2020-11-05 19:31:31 +0900

    Complete QuadrantN

--- mission1/src/com/company/QurantN/QuadrantN.java
+++ mission1/src/com/company/QurantN/QuadrantN.java
@@ -5,7 +5,20 @@ import java.util.Scanner;
 public class QuadrantN {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        num1 = s.nextInt();
-        num2 = s.nextInt();
+
+        int num1 = s.nextInt();
+        int num2 = s.nextInt();
+        if(num1 > 0 && num2 >0){
+            System.out.println(1);
+        }
+        if(num1 < 0 && num2 > 0){
+            System.out.println(2);
+        }
+        if(num1 < 0 && num2 < 0){
+            System.out.println(3);
+        }
+        if (num1 > 0 && num2 < 0){
+            System.out.println(4);
+        }
     }
 }
