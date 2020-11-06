03c66d315fc0b7bc2c33bafb2defe992ebcf693c
Nayeon <kimnayeon0108@users.noreply.github.com>
2020-11-06 11:38:21 +0900

    Create Backjoon2 and 3 file

---
+++ day5 baekjoon algorithm/Baekjoon1000/Backjoon2.java
@@ -0,0 +1,14 @@
+package Baekjoon1000;
+
+import java.util.Scanner;
+
+public class Backjoon2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+
+        double c = (double) a / b;
+        System.out.println(c);
+    }
+}
