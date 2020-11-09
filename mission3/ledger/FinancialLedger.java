0f5b5074f424acee7d04a2c47a152030dc571896
Nayeon <kimnayeon0108@users.noreply.github.com>
2020-11-09 16:30:04 +0900

    Modify FinancialLedger.java

--- mission3/ledger/FinancialLedger.java
+++ mission3/ledger/FinancialLedger.java
@@ -1,11 +1,42 @@
 package ledger;
 
+import java.util.Scanner;
+
+class Data {
+    String date;
+    String note;
+    int amount;
+    int num = 1;
+
+    public void inputData() {
+        System.out.println("---- " + num + " 번----");
+        System.out.println("날짜, 적요, 금액을 입력하시오. ");
+        Scanner s = new Scanner(System.in);
+        date = s.next();
+        note = s.next();
+        amount = s.nextInt();
+    }
+}
+
+class User {
+    static String name;
+    static int password;
+}
+
 public class FinancialLedger {
+
     public static void main(String[] args) {
-        FinancialLedger ledger = new FinancialLedger();
-//        public void dataInput(){
-//            System.out.println("날짜, 적요, 금액을 입력하세요");
-//        }
-//        ledger.dataInput();
+        User user = new User();
+        Data data = new Data();
+
+        System.out.println("이름과 비밀번호를 입력하세요.");
+        Scanner s = new Scanner(System.in);
+        user.name = s.next();
+        user.password = s.nextInt();
+
+        data.inputData();
+
+
+
     }
 }
--- mission3/ledger/User.java
+++
@@ -1,9 +0,0 @@
-package ledger;
-
-import java.util.Scanner;
-
-public class User {
-    Scanner s = new Scanner(System.in);
-    String name = s.next();
-    int password = s.nextInt();
-}
