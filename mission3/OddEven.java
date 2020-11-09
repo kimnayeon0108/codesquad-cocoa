431547c284b816cd292300df1a7082824c1f787b
Nayeon <kimnayeon0108@users.noreply.github.com>
2020-11-09 15:53:31 +0900

    Complete OddEven.java

--- mission3/OddEven.java
+++ mission3/OddEven.java
@@ -12,58 +12,90 @@ class Player2 {
 }
 
 class OddEven {
-    public static String result; //여기에 왜 static을 붙이면 되고 안붙이면 안되는
+    static String result; //여기에 왜 static을 붙이면 되고 안붙이면 안되는
+    static String choice;
+    static int numStage;
+    static int numTurn;
 
     public static String whatIsResult() {
         Random rand = new Random();
-        int num = rand.nextInt(20);
-        if (num % 2 == 0) {
+        int min = 1;
+        int max = 20;
+        int randomNum = rand.nextInt((max - min) + 1 + min);
+        if (randomNum % 2 == 0) {
             result = "짝";
         }
-        if (num % 2 == 1) {
+        if (randomNum % 2 == 1) {
             result = "홀";
         }
         return result;
     }
 
-
-    public static void main(String[] args) {
-
+    public static void play(){
         Player1 p1 = new Player1();
         Player2 p2 = new Player2();
-
-        System.out.println("이름을 입력하시오.");
         Scanner s = new Scanner(System.in);
-        p1.name = s.next();
 
-        while(p1.amount > 0) {
-            System.out.println("베팅금액을 입력하시오.");
+        while (p1.amount > 0 && p2.amount > 0) {
+            System.out.println("베팅금액을 입력하시오. 보유 금액: " + p1.amount);
             int bettingAmount = s.nextInt();
+            numTurn++;
 
-            if (bettingAmount > p1.amount) {
-                System.out.println("베팅금액을 다시 입력하시오. 가능 금액은: " + p1.amount);
+            if (bettingAmount > p1.amount || bettingAmount > p2.amount) {
+                System.out.println("베팅금액을 다시 입력하시오. 보유 금액: " + p1.amount
+                        + " 상대 보유 금액:" + p2.amount);
                 bettingAmount = s.nextInt();
             }
 
             System.out.println("홀 또는 짝을 입력하시오.");
-            String choice = s.next();
+            choice = s.next();
 
-            whatIsResult();
-            if (choice == result) {
+            if (choice.equals(whatIsResult())) {
                 System.out.println("맞았습니다!");
                 p1.amount += bettingAmount;
                 p2.amount -= bettingAmount;
                 System.out.println("보유 금액: " + p1.amount);
-            }
-            if (choice != result) {
+            } else {
                 System.out.println("틀렸습니다!");
                 p1.amount -= bettingAmount;
                 p2.amount += bettingAmount;
                 System.out.println("보유 금액: " + p1.amount);
             }
 
+            // 새로운 상대 생성
+            if (p2.amount == 0) {
+                numTurn = 0;
+                numStage++;
+                System.out.println("-----new game stage: " + numStage + "-----");
+                p2.amount = p1.amount * (int)Math.pow(1.2, numStage);
+                System.out.println("새로운 상대가 생성되었습니다. 상대의 보유 금액: " + p2.amount);
+            }
+            // player amount 소진시 게임 종료
+            if (p1.amount == 0) {
+                System.out.println("보유 금액이 0원입니다. 게임이 종료되었습니다.");
+                System.out.println("stage: " + (numStage) + " 시도 횟수: " + numTurn);
+            }
+
+            // 스테이지 정복 게임 종료
+            if (numStage == 9) {
+                System.out.println("게임이 종료되었습니다.");
+                System.out.println("stage: " + (numStage - 1) + " 시도 횟수: " + numTurn + " 보유 금액: " + p1.amount);
+            }
+
         }
-        System.out.println("게임이 종료되었습니다.");
+    }
+
+    public static void main(String[] args) {
+        Player1 p1 = new Player1();
+        Player2 p2 = new Player2();
+
+        System.out.println("이름을 입력하시오.");
+        Scanner s = new Scanner(System.in);
+        p1.name = s.next();
+        System.out.println("보유 금액: " + p1.amount);
+        numStage = 1;
+
+        play();
 
     }
 }
