f56177164163d5fbea26af38210abe5e59e93fbd
Nayeon <kimnayeon0108@users.noreply.github.com>
2020-11-25 14:38:37 +0900

    Create exam package, GraphicsEx1.java

--- week4_mission8/DrawingBoard.java
+++
@@ -1,8 +0,0 @@
-import java.awt.*;
-
-public class DrawingBoard {
-    public static void main(String[] args) {
-        Frame f = new Frame("Drawing board");
-        f.setSize(400, 500);
-    }
-}
