18d1b5b0eed120bfa37486e0bbe2f45e798a2082
Nayeon <kimnayeon0108@users.noreply.github.com>
2020-11-25 23:42:28 +0900

    Add itemStateChanged method with color but not working, need to modify

--- week4_mission8/drawingBoard/DrawingBoard.java
+++ week4_mission8/drawingBoard/DrawingBoard.java
@@ -1,21 +1,21 @@
 package drawingBoard;
 
+import javafx.event.EventHandler;
+
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
 
 public class DrawingBoard extends Frame implements MouseMotionListener {
     int x = 0;
     int y = 0;
-//    ScrollPane sp;
     Panel p;
+    Checkbox[] boxShape = new Checkbox[4];
+    Checkbox[] boxColor = new Checkbox[3];
 
     Image img = null;
     Graphics gImg = null;
 
-    public DrawingBoard(String title){
+    public DrawingBoard(String title) {
         super(title);
         addMouseMotionListener(this);
         addWindowListener(new WindowAdapter() {
@@ -25,7 +25,7 @@ public class DrawingBoard extends Frame implements MouseMotionListener {
         });
         Toolkit tk = Toolkit.getDefaultToolkit();
         Dimension screenSize = tk.getScreenSize();
-        setBounds(screenSize.width/2 -250, screenSize.height/2 -250, 500, 500);
+        setBounds(screenSize.width / 2 - 350, screenSize.height / 2 - 250, 700, 500);
         setResizable(true);
         setLayout(new FlowLayout());
 
@@ -34,68 +34,101 @@ public class DrawingBoard extends Frame implements MouseMotionListener {
 
         setVisible(true);
 
-        img = createImage(500, 500);
+        img = createImage(700, 500);
         gImg = img.getGraphics();
         repaint();
     }
 
-    public void panelInFrame(){
+    public void panelInFrame() {
 //        sp = new ScrollPane(ScrollPane.SCROLLBARS_AS_NEEDED);
 
         // checkbox 생성
         p = new Panel();
         p.setLayout(new BorderLayout(10, 10));
 
+        chooseShape();
+        chooseColor();
+    }
+
+    private void chooseShape() {
         // 그리기 패널
         Panel p1 = new Panel();
         Label l1 = new Label("그리기");
         CheckboxGroup group1 = new CheckboxGroup();
-        Checkbox line = new Checkbox("직선", group1, false);
-        Checkbox curve = new Checkbox("곡선", group1, false);
-        Checkbox square = new Checkbox("사각형", group1, false);
-        Checkbox circle = new Checkbox("원", group1, false);
-        p1.add(l1);
-        p1.add(line);
-        p1.add(curve);
-        p1.add(square);
-        p1.add(circle);
-        p1.setSize(200, 50);
 
+        p1.add(l1);
+
+        boxShape[0] = new Checkbox("직선", group1, false);
+        boxShape[1] = new Checkbox("곡선", group1, false);
+        boxShape[2] = new Checkbox("사각형", group1, false);
+        boxShape[3] = new Checkbox("원", group1, false);
+
+        for(int i = 0; i < boxShape.length; i++){
+            boxShape[i].addItemListener(new EventHandler());
+            p1.add(boxShape[i]);
+        }
+
+        p1.setSize(200, 50);
+        p1.setBackground(Color.lightGray);
+
+        p.add("West", p1);
+    }
+
+    private void chooseColor() {
         // 색상 패널
         Panel p2 = new Panel();
         Label l2 = new Label("color");
         CheckboxGroup group2 = new CheckboxGroup();
-        Checkbox black = new Checkbox("검정", group2, false);
-        Checkbox pink = new Checkbox("분홍", group2, false);
-        Checkbox blue = new Checkbox("파랑", group2, false);
+
         p2.add(l2);
-        p2.add(black);
-        p2.add(pink);
-        p2.add(blue);
 
-        p.add("West", p1);
+        boxColor[0] = new Checkbox("검정", group2, false);
+        boxColor[1] = new Checkbox("분홍", group2, false);
+        boxColor[2] = new Checkbox("파랑", group2, false);
+
+        for(int i = 0; i < boxColor.length; i++){
+            p2.add(boxColor[i]);
+        }
+
+        p2.setSize(200, 50);
+        p2.setBackground(Color.lightGray);
+
         p.add("East", p2);
-
     }
 
-    public void paint (Graphics g){
+
+    public void paint(Graphics g) {
         // 가상 화면에 그려민 그림을 Frame에 복사
-        if(img != null) g.drawImage(img, 0, 0, this);
+        if (img != null) g.drawImage(img, 0, 0, this);
     }
 
-    public void mouseMoved(MouseEvent me){
+    public void mouseMoved(MouseEvent me) {
         x = me.getX();
         y = me.getY();
     }
 
-    public void mouseDragged(MouseEvent me){
+    public void mouseDragged(MouseEvent me) {
         if (me.getModifiersEx() == MouseEvent.BUTTON1_DOWN_MASK) {
-
-            gImg.drawLine( x, y, me.getX(), me.getY());
+                gImg.drawLine(x, y, me.getX(), me.getY());
             x = me.getX();
             y = me.getY();
 
             repaint();
+        }
+    }
+
+    class EventHandler implements ItemListener{
+        @Override
+        public void itemStateChanged(ItemEvent e) {
+            Checkbox cb = (Checkbox)e.getSource();
+            String color = cb.getLabel();
+            if(color.equals("검정")){
+                gImg.setColor(Color.black);
+            } else if(color.equals("분홍")){
+                gImg.setColor(Color.pink);
+            } else if(color.equals("파랑")){
+                gImg.setColor(Color.blue);
+            }
         }
     }
 }
