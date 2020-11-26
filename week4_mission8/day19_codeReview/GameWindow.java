e80d072325e05842f187e9f435cf9970a505b298
Nayeon <kimnayeon0108@users.noreply.github.com>
2020-11-26 17:47:30 +0900

    Add background

--- week4_mission8/day19_codeReview/GameWindow.java
+++ week4_mission8/day19_codeReview/GameWindow.java
@@ -2,6 +2,7 @@ package day19_codeReview;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -15,7 +16,14 @@ public class GameWindow extends JFrame {
         lodeImage("/Users/kimnayeon/codesquad-cocoa/week4_mission8/resources/pexels-miguel-á-padriñán-255379.jpg");
         init();
         initForky();
+//        addEvent();
     }
+
+//    private void addEvent() {
+//        input = new Input();
+//        addMouseListener(input);
+//        addMouseMotionListener(input);
+//    }
 
     private void initForky() {
         forky = new Forky();
@@ -30,15 +38,22 @@ public class GameWindow extends JFrame {
     }
 
     private void lodeImage(String path) {
-        File f = new File("./");        // 여기 이해 안감.. 인자로 받은게 무엇을 의미하는지 ..
+//        File f = new File(path);        // 여기 이해 안감.. 인자로 받은게 무엇을 의미하는지 ..
         try {
             background = ImageIO.read(new File(path));
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(1);
         }
+    }
 
+    public void paint(Graphics g){
+        g.drawImage(background, 0, 0, this);
+    }
 
+    public static void main(String[] args) {
+        GameWindow frame = new GameWindow();
+        frame.setVisible(true);
     }
 
 }
